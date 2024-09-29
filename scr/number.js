9f6b838919682d958cb58f4506027e2277f8d556
TailBear1107 <143160850+TailBear1107@users.noreply.github.com>
2024-09-29 21:42:31 +0800

    Update number.js

--- scr/number.js
+++ scr/number.js
@@ -1,3 +1,5 @@
+var start,end,count,vok;
+
 function numsort(a,b){
     if(a>b){
         return 1;
@@ -9,48 +11,61 @@ function numsort(a,b){
 }
 //數字比較的函式
 
-window.addEventListener("keyup",function(event){
-    event.preventDefault();
-    if (event.keyCode === 13) {
-    document.getElementById("start_button").click();
-    }
-})
-//增加一個EventListener，偵測Enter鍵是否被按下，若被按下則代為執行點擊按鈕
-
-var el = document.querySelector("#start_button");
-el.onclick = function proc(){
-    var start,end,count;
+function getvalue(){
     var inputElement = document.getElementById("input_start");
     start = inputElement.valueAsNumber;
     inputElement = document.getElementById("input_end");
     end = inputElement.valueAsNumber;
     inputElement = document.getElementById("input_count");
     count = inputElement.valueAsNumber;
-    if(start % 1 != 0){
-        window.alert("輸入數字需為整數");
-        document.getElementById("input_start").value = "";
+}
+//讀取輸入數值
+
+document.querySelectorAll(".input_num").forEach(function(element) {
+    element.addEventListener("input", function(){
+        getvalue();
+        if(start % 1 != 0 || end % 1 != 0 || count % 1 != 0) {
+            document.getElementById("start_button").setAttribute("disabled", "disabled");
+            document.getElementById("start_button").setAttribute("title", "檢查輸入數值是否完整為整數");
+            document.getElementById("start_button").style.cursor = "not-allowed";
+            vok = false;
+            document.getElementById("status").textContent = "檢查輸入數值是否完整為整數";
+        }else if(start > Number.MAX_SAFE_INTEGER || end > Number.MAX_SAFE_INTEGER || count > Number.MAX_SAFE_INTEGER) {
+            document.getElementById("start_button").setAttribute("disabled", "disabled");
+            document.getElementById("start_button").setAttribute("title", "輸入數值過大或欲產生的數字過多！");
+            document.getElementById("start_button").style.cursor = "not-allowed";
+            vok = false;
+            document.getElementById("status").textContent = "輸入數值過大或欲產生的數字過多！";
+        }else if(count <= 0){
+            document.getElementById("start_button").setAttribute("disabled", "disabled");
+            document.getElementById("start_button").setAttribute("title", "欲產生的數字不可小於1個！");
+            document.getElementById("start_button").style.cursor = "not-allowed";
+            vok = false;
+            document.getElementById("status").textContent = "欲產生的數字不可小於1個！";
+        }else{
+            document.getElementById("start_button").removeAttribute("disabled","");
+            document.getElementById("start_button").setAttribute("title", "產生隨機數字");
+            document.getElementById("start_button").style.cursor = "pointer";
+            vok = true;
+            document.getElementById("status").textContent = "";
+        }
+    })
+})
+//檢查輸入數值
+
+window.addEventListener("keyup",function(event){
+    if (event.key == "Enter") {
+        if(vok == true){
+            document.getElementById("start_button").click();
+        }
     }
-    if(start > Number.MAX_SAFE_INTEGER){
-        window.alert("輸入數字過大！");
-        document.getElementById("input_start").value = "";
-    }
-    if(end % 1 != 0){
-        window.alert("輸入數字需為整數");
-        document.getElementById("input_end").value = "";
-    }
-    if(end > Number.MAX_SAFE_INTEGER){
-        window.alert("輸入數字過大！");
-        document.getElementById("input_end").value = "";
-    }
-    if(count <= 0 || count % 1 != 0){
-        window.alert("欲產生的數字數量有誤，請修正");
-        document.getElementById("input_count").value = "";
-    }
-    if(count > Number.MAX_SAFE_INTEGER){
-        window.alert("欲產生的數字數量過多！");
-        document.getElementById("input_count").value = "";
-    }
-    //讀值進來
+    event.preventDefault();
+})
+//增加一個EventListener，偵測Enter鍵是否被按下，若被按下則代為執行點擊按鈕
+
+var el = document.querySelector("#start_button");
+el.onclick = function proc(){
+    getvalue();
 
     var numarr = [];
     var result = [];
@@ -69,12 +84,12 @@ el.onclick = function proc(){
     var num;
     while(result.length < count){
         var acrepeat = document.getElementById("acceptrepeat");
-        num = numarr[Math.floor(Math.random() * numarr.length)];//向下取整
+        num = numarr[Math.floor(getran() * numarr.length)];//向下取整
         if(acrepeat.checked){
             result.push(num);
         }else{
             if(count > Math.abs(start-end+1)){
-                window.alert("不產生重複數字之數量僅可不超過戴選擇數字數量！");
+                window.alert("不產生重複數字之數量僅可不超過待選擇數字數量！");
                 document.getElementById("input_count").value = "";
                 break;
             }else{
